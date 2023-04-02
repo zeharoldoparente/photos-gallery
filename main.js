c152c1ce3bfb9df25a6401c351a454143519d90b
Zé Aroldo <joseharoldoparente@gmail.com>
2023-04-02 18:50:08 -0400

    inicializando o jQuery

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('header button').click(function () {
+        alert('Expandir formulário');
+    });
+
+    $('form').on('submit',function(e) {
+        console.log("submit");
+        e.preventDefault();
+    });
+});
